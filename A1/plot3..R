9f8700483fa9e440d62f22d8b1565689ab26e744
Robert Palumbo <rpalumbo.co@gmail.com>
2015-09-11 17:52:45 -0600

    Adjusted legend labels

--- A1/plot3..R
+++ A1/plot3..R
@@ -25,14 +25,16 @@ p3SubData$Tmstmp <- as.POSIXct(paste(p3SubData$Date, p3SubData$Time),
                                format = "%d/%m/%Y %H:%M:%S")
 
 message("Plot a line-graph for each Sub_metering_[1..3]...")
-plot(p3SubData$Tmstmp,  p3SubData$Sub_metering_1, type = "l", 
+plot(p3SubData$Tmstmp,  p3SubData$Sub_metering_1, type = "l",
      ylab = "Energy sub metering", xlab = "", col="black")
 lines(p3SubData$Tmstmp, p3SubData$Sub_metering_2,col="red")
 lines(p3SubData$Tmstmp, p3SubData$Sub_metering_3,col="blue")
 
 # Create a legend
-legend("topright", lty = 1, lwd=2, pt.cex=1, cex=0.75, 
+legend("topright", lty = 1, lwd=2, cex = 0.7,
+       x.intersp = 0.25, y.intersp = 0.5,
        col=c("black", "red", "blue"), 
+       text.width = strwidth("100,000"),
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 message("Save the plot as 'plot3.png'")
