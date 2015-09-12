a02e95ecdfbb5bed2bb0aefed1b7f53d2cdde36d
Robert Palumbo <rpalumbo.co@gmail.com>
2015-09-11 21:54:07 -0600

    Plotting format changes. Working version of plot4.R.

--- A1/plot1.R
+++ A1/plot1.R
@@ -21,14 +21,19 @@ p1Data$Date <- as.Date(p1Data$Date, format = "%d/%m/%Y")
 message("Filter the data for the specified range [02/01/2007 - 02/02/2007]")
 p1SubData <- filter(p1Data, Date >= "2007-02-01" & Date < "2007-02-03")
 
+# Slightly adjust the plot margins
+par1 <- par(mai=c(0.75, 0.9, 0.5, 0.5))
+
 message("Plot a histogram for the Global_active_power variable...")
 hist(p1SubData$Global_active_power, freq = TRUE, col = "red",
      main = "Global Active Power", xlab = "Global Active Power (killowatts)")
 
 message("Save the plot as 'plot1.png'")
 
+par(mai=par1)
+
 # Save plot to png file
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot1.png", width = 640, height = 480)
 dev.off()
 
 message("Done!")
